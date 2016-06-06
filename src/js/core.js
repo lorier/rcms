e88796bd66d8adff00ad29b5632cc82b0e0497b8
Lorie Ransom <lorie@ransomdesign.com>
2016-06-06 13:03:15 -0700

    mystery shopping subpages start

--- src/js/core.js
+++ src/js/core.js
@@ -11,14 +11,3 @@
 	});
 }(jQuery));
 
-
-// A simple wrapper for all your custom jQuery; everything in this file will be run on every page
-// ;(function($){
-//   $(document).ready(function() {
-// 	console.log('scroll');
-// 		$('.site-header').scrollToFixed();
-// 	});
-//   $(function(){
- 
-//   });
-// }(jQuery));
